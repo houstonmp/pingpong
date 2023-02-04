c73f3399345275d2d33363f04bcac97858bb3da7
Matthew Houston <matthouston027@gmail.com>
2023-02-05 03:56:07 +0900

    Finish JS to add points and satisfy win conditions, Add Css for Win-Lose, Add Reset Button

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
 const select = document.querySelector("#select");
-let score = 5;
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const scoreSpan1 = document.querySelector("#scoreSpan1");
+const scoreSpan2 = document.querySelector("#scoreSpan2");
+let winScore = 5;
+let score1 = 0;
+let score2 = 0;
 
 
 select.addEventListener("input", (e) => {
     e.preventDefault();
-    score = select.value;
-    console.log(score);
+    winScore = parseInt(select.value);
+    console.log(winScore);
+});
+
+player1.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Player:" + winScore);
+
+    score1++;
+    scoreSpan1.textContent = score1;
+    if (score1 === winScore) {
+        scoreSpan1.classList.add("win");
+        scoreSpan2.classList.add("lose");
+    }
+});
+
+player2.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("Player:" + winScore);
+
+    score2++;
+    scoreSpan2.textContent = score2;
+    if (score2 === winScore) {
+        scoreSpan2.classList.add("win");
+        scoreSpan1.classList.add("lose");
+    }
+});
+
+reset.addEventListener("click", (e) => {
+    e.preventDefault();
+    winScore = 5;
+    winScore.value = 5;
+    score1 = 0;
+    score2 = 0;
+    scoreSpan1.innerText = 0;
+    scoreSpan2.innerText = 0;
+    scoreSpan1.classList.contains("win") && scoreSpan1.classList.remove("win");
+    scoreSpan1.classList.contains("lose") && scoreSpan1.classList.remove("lose");
+    scoreSpan2.classList.contains("win") && scoreSpan2.classList.remove("win");
+    scoreSpan2.classList.contains("lose") && scoreSpan2.classList.remove("lose");
 });
 
